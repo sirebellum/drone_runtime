5e3d1acfe08b5213297292d18f1698dac06f327e
Josh Herrera <joshua.herrera227@gmail.com>
2022-12-06 23:15:34 -0600

    Packet and shipment capabilities

--- fc_firmware/include/io/spi.h
+++ fc_firmware/include/io/spi.h
@@ -4,16 +4,24 @@
 #include <sys/ioctl.h>
 #include <wiringPiSPI.h>
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 1024 + 16 // for packet metadata
+
+struct SPI_PACKET {
+  uint32_t len;
+  uint32_t idx;
+  unsigned char* buffer;
+};
 
 class SPI {
 public:
   SPI();
   ~SPI();
   int fd;
-  int readwrite();
-  unsigned char _buffer[BUFFER_SIZE];
-  unsigned char* buffer; 
+  void rawReadWrite();
+  int readBuffer(unsigned char* buffer);
+  uint32_t shipmentReceive(unsigned char* buffer); // Returns size of packet in bytes
 
 private:
+  unsigned char _buffer[BUFFER_SIZE];
+  unsigned char* buffer = _buffer;
 };
--- fc_firmware/src/io/spi.cpp
+++ fc_firmware/src/io/spi.cpp
@@ -15,7 +15,34 @@ SPI::SPI() {
 
 SPI::~SPI() { close(this->fd); }
 
-int SPI::readwrite() {
+int SPI::readBuffer(unsigned char* buffer) {
+  wmemcpy(reinterpret_cast<wchar_t*>(buffer),
+          reinterpret_cast<wchar_t*>(this->buffer),
+          BUFFER_SIZE);
+  return BUFFER_SIZE;
+}
+
+void SPI::rawReadWrite() {
   wiringPiSPIDataRW(SPI_CHANNEL, this->buffer, BUFFER_SIZE);
 }
 
+uint32_t SPI::shipmentReceive(unsigned char* buffer) {
+
+  // Receiving and handling
+  this->rawReadWrite();
+  SPI_PACKET* packet = reinterpret_cast<SPI_PACKET*>(this->buffer);
+  uint32_t len = packet->len;
+  uint32_t idx = 0;
+
+  // Setup buffer for shipment
+  buffer = new unsigned char(len);
+  
+  while (idx <= len) {
+    this->rawReadWrite();
+    wmemcpy(reinterpret_cast<wchar_t*>(buffer[idx*BUFFER_SIZE]),
+            reinterpret_cast<wchar_t*>(this->buffer),
+            BUFFER_SIZE);
+    idx = packet->idx;
+  }
+  return len*BUFFER_SIZE;
+}
