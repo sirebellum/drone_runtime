14e3c7742972e8ec2aad873d616f63106bc76468
Josh Herrera <joshua.herrera@quantcivil.ai>
2023-09-16 14:34:20 +0000

    Remove old build

--- fc_firmware/build.sh
+++
@@ -1,8 +0,0 @@
-LIBS+="-lcurlpp -lcurl"
-LIBS+=" /usr/local/lib/libtvm_runtime.so"
-LIBS+=" /usr/lib/x86_64-linux-gnu/libopencv*.so"
-INCLUDES+=" -Iinclude/"
-INCLUDES+=" -I/home/compute/gits.local/tvm/3rdparty/dlpack/include"
-INCLUDES+=" -I/home/compute/gits.local/tvm/3rdparty/dmlc-core/include"
-
-clang++ main.cpp src/*.cpp src/*/*.cpp $INCLUDES $LIBS -std=c++17 -fuse-ld=lld -o firmware
